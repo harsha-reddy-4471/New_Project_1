50b3ed140a24015c00f07e27e6c052a756a82a4e
Harsha <harshareddy4471@gmail.com>
2024-03-08 21:58:17 +0530

    adding all files

---
+++ src/main/java/com/cts/MyFirstApp011Application.java
@@ -0,0 +1,16 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MyFirstApp011Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(MyFirstApp011Application.class, args);
+		System.out.println("welcome harsha");
+	}
+
+}
